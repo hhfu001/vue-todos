e6c4b13101b6720f445401bfe89ceb7be9731aa1
hhfu <hhfu@tudou.com>
2016-11-28 18:26:55 +0800

    点击文字可编辑

--- src/components/todoItem/todoItem.js
+++ src/components/todoItem/todoItem.js
@@ -2,16 +2,31 @@ export default {
     props: ['todo', 'index'],
     data() { 
         return {
-            done: {
+            doneStyle: {
                 'text-decoration': 'line-through',
                 'color': '#666'
-            }
+            },
+            isEdit: false
         }
     },
     
     methods: {
         handlerChange() { 
             this.todo.isDone = !this.todo.isDone; 
+
+            this.$emit('stateChange', this.index, this.todo);
+        },
+        edit() { 
+            this.isEdit = true;
+            this.originText = this.todo.text;
+        },
+        cancle() { 
+            this.isEdit = false;
+            this.todo.text = this.originText;
+        },
+
+        save() { 
+            this.isEdit = false;
 
             this.$emit('stateChange', this.index, this.todo);
         },
--- src/components/todoItem/todoItem.vue
+++ src/components/todoItem/todoItem.vue
@@ -1,7 +1,11 @@
 <template>
     <li>
         <input :checked="todo.isDone" type="checkbox" @change="handlerChange"/>
-        <span :style="[todo.isDone ? done: '']">{{todo.text}}</span>
+        
+        <span :style="[todo.isDone ? doneStyle: '']" v-if="!isEdit"  @click="edit" >{{todo.text}}</span>
+        <span v-else><input autofocus v-model="todo.text" @keyup.enter="save" type="text" /><button @click="cancle">取消</button></span>
+
+
         <button class="fr" @click="delTodo">删除</button>
     </li>
 </template>
